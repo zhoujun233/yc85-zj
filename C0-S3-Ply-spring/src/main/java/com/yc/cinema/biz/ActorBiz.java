80f4ac83e0e44b432bec11c7b30434db19f9157f
zhoujun233 <1529072552@qq.com>
2020-08-11 14:56:19 +0800

    电影院工程

---
+++ C0-S3-Ply-spring/src/main/java/com/yc/cinema/biz/ActorBiz.java
@@ -0,0 +1,17 @@
+package com.yc.cinema.biz;
+
+import com.yc.cinema.dao.ActorDao;
+
+public class ActorBiz {
+	
+	private ActorDao adao;
+
+	public ActorDao getAdao() {
+		return adao;
+	}
+
+	public void setAdao(ActorDao adao) {
+		this.adao = adao;
+	}
+	
+}
--- C85-S3-ZJ-Spring/src/main/java/com/zj/spring/dao/MySqlUserDao.java
+++ C85-S3-ZJ-Spring/src/main/java/com/zj/spring/dao/MySqlUserDao.java
@@ -4,7 +4,10 @@ import org.springframework.stereotype.Repository;
 /**
  * 注解方式的自动装配
  * @author Administrator
- *
+ *@Component   通用的组件注解
+ *@Controller  控制器组件注解 ===>相当于Servlet
+ *@Service     服务类组件注解===>xxxBiz,xxxService
+ *@Repository  dao类组件注解===>xxxDao,xxxMapper
  */
 
 @Repository("mdao")
