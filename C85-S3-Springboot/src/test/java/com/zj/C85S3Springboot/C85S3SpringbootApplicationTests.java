0f8c9282f49aa3204e3f892c81d31f93a9b6db22
zhoujun233 <1529072552@qq.com>
2020-08-20 12:37:42 +0800

    日志配置，邮件发送

--- C85-S3-Springboot/src/test/java/com/zj/C85S3Springboot/C85S3SpringbootApplicationTests.java
+++ C85-S3-Springboot/src/test/java/com/zj/C85S3Springboot/C85S3SpringbootApplicationTests.java
@@ -6,6 +6,7 @@ import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.util.Assert;
 
+import com.zj.C85S3Springboot.biz.SendMail;
 import com.zj.C85S3Springboot.dao.DmProductMapper;
 
 @SpringBootTest
@@ -13,10 +14,19 @@ class C85S3SpringbootApplicationTests {
 	
 	@Resource
 	DmProductMapper mapper;
+	
+	@Resource
+	SendMail sm;
 
 	@Test
 	void contextLoads() {
 		Assert.isTrue(mapper.selectAll().size()>0, "没有数据");
 	}
+	
+	@Test
+	void testMail() {
+		sm.sendSimpleMail("1529072552@qq.com", "登录验证码", "你好，你的登录验证码为12005");
+	}
+
 
 }
