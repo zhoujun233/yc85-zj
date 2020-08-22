e3a429d237168351a3cd4d142360d9f0dcf41ce6
zhoujun233 <1529072552@qq.com>
2020-08-22 22:28:04 +0800

    客服和定时任务

--- C85-S3-Springboot/src/main/java/com/zj/C85S3Springboot/C85S3SpringbootApplication.java
+++ C85-S3-Springboot/src/main/java/com/zj/C85S3Springboot/C85S3SpringbootApplication.java
@@ -3,14 +3,23 @@ package com.zj.C85S3Springboot;
 import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Bean;
+import org.springframework.scheduling.annotation.EnableScheduling;
+import org.springframework.web.socket.server.standard.ServerEndpointExporter;
 
 @SpringBootApplication
 @MapperScan("com.zj.C85S3Springboot.dao")
+//开启定时任务
+@EnableScheduling
 public class C85S3SpringbootApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(C85S3SpringbootApplication.class, args);
 	}
 
+	@Bean
+	public ServerEndpointExporter serverEndpointExporter() {
+		return new ServerEndpointExporter();
+		
+	}
 }
---
+++ C85-S3-Springboot/src/main/java/com/zj/C85S3Springboot/biz/MyWebScoket.java
@@ -0,0 +1,58 @@
+package com.zj.C85S3Springboot.biz;
+
+import java.io.IOException;
+import java.util.Hashtable;
+
+import javax.websocket.OnClose;
+import javax.websocket.OnMessage;
+import javax.websocket.OnOpen;
+import javax.websocket.Session;
+import javax.websocket.server.PathParam;
+import javax.websocket.server.ServerEndpoint;
+
+import org.springframework.scheduling.annotation.Scheduled;
+import org.springframework.stereotype.Component;
+
+@ServerEndpoint(value = "/websocket/{id}")
+@Component
+public class MyWebScoket {
+	
+	private static Hashtable<String, Session>
+	websocketMap=new Hashtable<>();
+	
+	@OnOpen//连接建立成功调用的方法
+	public void onOpen(@PathParam("id")String id,Session session) {
+		//Hashtable(线程安全)    HashMap(线程不安全)
+		System.out.println(id+"连接成功");
+		websocketMap.put(id, session);
+		
+	}
+
+	@OnClose//连接关闭调用的方法
+	public void onClose(Session session) {
+		
+		
+	}
+	
+	@OnMessage//接收到客户端消息后调用的方法
+	public void onMessage(String message,Session session) throws IOException {
+		//
+		String ss[]=message.split(":");
+		String id=ss[0];
+		String msg=ss[1];
+		Session targetSession =websocketMap.get(id);
+		if(targetSession!=null) {
+			targetSession.getBasicRemote().sendText(msg);
+		}else {
+			System.out.println(id+"不在线");
+		}
+		
+	}
+	
+	@Scheduled(cron = "0 */2 * * * ?")//每两分钟执行一次
+	public void luckyou() throws IOException {
+		for(Session session:websocketMap.values()) {
+			session.getBasicRemote().sendText("祝你好运");
+		}
+	}
+}
