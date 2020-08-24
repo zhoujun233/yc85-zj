c5402f7ecd0b17a22ebd983f7cbace6074be3740
zhoujun233 <1529072552@qq.com>
2020-08-24 09:32:59 +0800

    登录，注册业务

---
+++ C85-S3-Blog/src/main/java/com/zj/C85S3Blog/biz/UserBiz.java
@@ -0,0 +1,38 @@
+package com.zj.C85S3Blog.biz;
+
+import javax.annotation.Resource;
+
+import org.springframework.stereotype.Service;
+
+import com.zj.C85S3Blog.bean.User;
+import com.zj.C85S3Blog.dao.UserMapper;
+
+@Service
+public class UserBiz {
+
+	@Resource
+	private UserMapper umapper;
+
+	public void reg(User user) throws BizException {
+		// 可以忽略字段验证 数据校验
+		// 同名验证
+		if (umapper.selectAccount(user.getAccount()) > 0) {
+			throw new BizException("该用户名已存在");
+		}
+
+		umapper.insert(user);
+
+	}
+
+	public User Login(User user) throws BizException {
+
+		User duser = umapper.selectByLogin(user);
+
+		if (duser == null) {
+			throw new BizException("用户名或密码错误");
+		}
+		return duser;
+
+	}
+
+}
---
+++ C85-S3-Blog/src/main/java/com/zj/C85S3Blog/dao/UserMapper.java
@@ -0,0 +1,20 @@
+package com.zj.C85S3Blog.dao;
+
+import org.apache.ibatis.annotations.Insert;
+import org.apache.ibatis.annotations.Select;
+
+import com.zj.C85S3Blog.bean.User;
+
+public interface UserMapper {
+	
+	@Insert("insert into values(null,#{name},#{account},#{pwd},#{phone},"
+			+ "#{email},#{head},now(),#{status},#{type})")
+	public void insert(User user);
+	
+	@Select("select * from user where account=#{account} and pwd=#{pwd}")
+	public User selectByLogin(User user);
+	
+	@Select("select count(*) from user where account=#{account}")
+	public int selectAccount(String account);
+
+}
