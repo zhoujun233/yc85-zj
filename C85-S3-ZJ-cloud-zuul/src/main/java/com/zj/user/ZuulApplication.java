4215b6563621ee99bb1459473f01fe0473c1cce3
zhoujun233 <1529072552@qq.com>
2020-08-28 17:05:45 +0800

    zuul路由网关配置

---
+++ C85-S3-ZJ-cloud-zuul/src/main/java/com/zj/user/ZuulApplication.java
@@ -0,0 +1,30 @@
+package com.zj.user;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.loadbalancer.LoadBalanced;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.cloud.netflix.zuul.EnableZuulProxy;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.client.RestTemplate;
+
+@EnableEurekaClient
+@EnableZuulProxy
+@SpringBootApplication
+public class ZuulApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ZuulApplication.class, args);
+	}
+
+
+	/**
+	 * 	定义 RestTemplate  Bean
+	 */
+	@LoadBalanced
+	@Bean
+	public RestTemplate getRestTemplate() {
+		return new RestTemplate();
+	}
+
+}
